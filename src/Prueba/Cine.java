0781b00b21a48e8aaf35101307f58a9f0c284656
netsergiosm <netsergiosm@a0b71201-70d1-97be-5837-091e657cf1f5>
2010-12-06 23:49:20 +0000

    Se obtiene la información de cada cine, menos los pases de las películas.

--- src/Prueba/Cine.java
+++ src/Prueba/Cine.java
@@ -2,40 +2,104 @@ package Prueba;
 
 import java.util.ArrayList;
 
+import Prueba.ProvinciasGDO.Provincia;
+
 public class Cine {
 	
 	//plantear la opción de crear la clase "pases" (que contenga peli, cine, día de la semaa y hora) si se piensa guardar tambien la hora de cada pase
 	//en ese caso, el arraylist seria de pases y no de pelis
-	private ArrayList<Pelicula> pelis;
+	private String dirWebGDO;
 	private String nombre;
-	// localidad como enumerado????
-	private String localidad;
+	private Provincia provincia;
+	private int numSalas;
+	private String tfno;
+	private String webOficial;
+	private String direccion;
+	private String zona;
+	private String parking;
+	private String urlCompras;
 	
 	
-	public Cine(String nombre, String localidad) {
+	
+	public String getUrlCompras() {
+		return urlCompras;
+	}
+
+
+	public void setUrlCompras(String urlCompras) {
+		this.urlCompras = urlCompras;
+	}
+
+
+	public String getParking() {
+		return parking;
+	}
+
+
+	public void setParking(String parking) {
+		this.parking = parking;
+	}
+
+
+	public String getZona() {
+		return zona;
+	}
+
+
+	public void setZona(String zona) {
+		this.zona = zona;
+	}
+
+
+	public Cine(String dirWebGDO, String nombre) {
+		super();
+		this.dirWebGDO = dirWebGDO;
 		this.nombre = nombre;
-		this.localidad = localidad;
-		pelis= new ArrayList<Pelicula>();
 	}
-	
-	public Pelicula getPeli(int numPeli) {
-		if (numPeli<pelis.size() && numPeli>0)
-			return pelis.get(numPeli);
-		else
-			return null;
+
+
+	public String getDirWebGDO() {
+		return dirWebGDO;
 	}
-	
+
+
+	public Provincia getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(Provincia provincia) {
+		this.provincia = provincia;
+	}
+	public int getNumSalas() {
+		return numSalas;
+	}
+	public void setNumSalas(int numSalas) {
+		this.numSalas = numSalas;
+	}
+	public String getTfno() {
+		return tfno;
+	}
+	public void setTfno(String tfno) {
+		this.tfno = tfno;
+	}
+	public String getWebOficial() {
+		return webOficial;
+	}
+	public void setWebOficial(String webOficial) {
+		this.webOficial = webOficial;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
 	public String getNombre() {
 		return nombre;
 	}
 	
-	public String getLocalidad() {
-		return localidad;
-	}
 	
-	public void addPeli(Pelicula peli){
-		pelis.add(peli);
-	}
+	
+	
 	
 
 }
