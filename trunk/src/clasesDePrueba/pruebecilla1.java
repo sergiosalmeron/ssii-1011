e57bd71451a80c4fb5bae391b696a760b9ffe88f
netsergiosm <netsergiosm@a0b71201-70d1-97be-5837-091e657cf1f5>
2010-12-09 20:48:03 +0000

    Se ha añadido el att sinopsis a las películas y se obtiene de la web.

--- trunk/src/clasesDePrueba/pruebecilla1.java
+++ trunk/src/clasesDePrueba/pruebecilla1.java
@@ -4,22 +4,25 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import extractores.cines.ProcesadorCinesGDO;
+import extractores.peliculas.ProcesadorCarteleraGDO;
 
 import tads.Cine;
+import tads.Pelicula;
 import tads.ProvinciasGDO.Provincia;
 
 public class pruebecilla1 {
 
 	public static void main(String[] args) throws IOException {
 
-		ProcesadorCinesGDO a=new ProcesadorCinesGDO();
+	/*	ProcesadorCinesGDO a=new ProcesadorCinesGDO();
 		ArrayList<Cine> cines=a.getCines(Provincia.avila);
 		for (Cine cine: cines)
-			System.out.println(cine);
-	/*	ProcesadorCarteleraGDO carte= new ProcesadorCarteleraGDO();
+			System.out.println(cine);*/
+		ProcesadorCarteleraGDO carte= new ProcesadorCarteleraGDO();
 		ArrayList<Pelicula> pelis=carte.getPeliculas(Provincia.avila);
 		for (Pelicula peli : pelis){
 			System.out.println(peli);
-		}*/
+			System.out.println(peli.getSinopsis());
+		}
 	}
 }
