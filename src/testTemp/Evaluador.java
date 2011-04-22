dbf961aa1555de59b758994a9e1e77cd8ee826c4
netsergiosm <netsergiosm@a0b71201-70d1-97be-5837-091e657cf1f5>
2011-04-22 12:08:38 +0000

    Tocado el evaluador e implementado un método que devuelve los porcentajes en un arraylist dentro de testemperamento

--- src/testTemp/Evaluador.java
+++ src/testTemp/Evaluador.java
@@ -8,7 +8,25 @@ public class Evaluador {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//Se deben pasar las respuestas como args separadas por espacio "0 0 1 3 2 1 1 2 1 0 3 ... 3"
+		//Se deben pasar las respuestas como args separadas por espacio "0 0 1 3 2 1 1 2 1 0 3 ..."
+		/*if (args.length==16){
+			TestTemperamento test= new TestTemperamento();
+			ArrayList<Pregunta> preguntas=test.getPreguntas();
+			int i=0;
+			for (Pregunta p : preguntas) {
+				//System.out.println("-----------Pr"+i+"---------------");
+				//System.out.println(p.toString());
+				//aux=keyboard.nextInt();
+				p.setRespuestaElegida(Integer.parseInt(args[i]));
+				i++;
+			}
+		}*/
+		System.out.println(evalua1("0 0 0 0 0 0 0 0 0 0 0 0 0 0 3 1"));
+	}
+	
+	public static ArrayList<Double>  evalua1 (String arg) {
+		String[] args=arg.split(" ");
+		//Se deben pasar las respuestas como args separadas por espacio "0 0 1 3 2 1 1 2 1 0 3 ..."
 		if (args.length==16){
 			TestTemperamento test= new TestTemperamento();
 			ArrayList<Pregunta> preguntas=test.getPreguntas();
@@ -20,9 +38,13 @@ public class Evaluador {
 				p.setRespuestaElegida(Integer.parseInt(args[i]));
 				i++;
 			}
+			return test.dameArrayListResultado();
 		}
-		
+		else
+			return null;
 
 	}
+	
+	
 
 }
